2687ba6d835ad4d1a4db70da07d1e2c2779f78a5
anbut02 <49370690+anbut02@users.noreply.github.com>
2019-04-08 11:14:26 +0530

    Updates

---
+++ precache-manifest.246b2be4f9042033e1c2cb854e322e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc5283d94148da069954",
+    "url": "/app204/static/js/runtime~main.a54d8aac.js"
+  },
+  {
+    "revision": "bf9052fed29e7441d65f",
+    "url": "/app204/static/js/main.40cdf42e.chunk.js"
+  },
+  {
+    "revision": "b9dcbdfd5ef8b8babe2b",
+    "url": "/app204/static/js/2.15911f45.chunk.js"
+  },
+  {
+    "revision": "bf9052fed29e7441d65f",
+    "url": "/app204/static/css/main.afa68263.chunk.css"
+  },
+  {
+    "revision": "b9dcbdfd5ef8b8babe2b",
+    "url": "/app204/static/css/2.d3ba6790.chunk.css"
+  },
+  {
+    "revision": "e531a09d08203d0a01ff4aaf8b72858a",
+    "url": "/app204/index.html"
+  }
+];
